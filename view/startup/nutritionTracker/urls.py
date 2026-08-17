5fe04cc9913513956d684a30952d02246240ffdb
connorkolodziej1-hue <connorkolodziej1@gmail.com>
2026-08-17 16:17:49 -0600

    Django working for front-end prototype

--- view/startup/nutritionTracker/urls.py
+++ view/startup/nutritionTracker/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/nutFactsResult", views.nutritionFacts),
+    path("", views.startPage),
+    path("nutFactsResult/", views.nutritionFacts),
 ]
--- view/startup/nutritionTracker/views.py
+++ view/startup/nutritionTracker/views.py
@@ -5,5 +5,14 @@ import datetime
 # Create your views here.
 def nutritionFacts(request):
     file = request.POST["file"]
-    new_facts = NutritionFacts(file=file, user=request.user, timestamp=datetime.datetime.now())
+    if request.user.is_authenticated:
+        new_facts = NutritionFacts(
+            file=file,
+            user=request.user,
+            timestamp=datetime.datetime.now()
+        )
     return HttpResponse("<h1>File received</h1>")
+
+def startPage(request):
+    context = {}
+    return render(request, "nutritionFactsReader.html", context)
